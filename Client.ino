649373e13eccdde7e5edfd30894b7ab5656e8d01
SarahCheng <bigbigtreeful@gmail.com>
2017-08-12 18:08:18 +0800

    Add analog input
    Add A0, D8(pt for A0 is used or not)

--- Client.ino
+++ Client.ino
@@ -11,14 +11,21 @@ WiFiClient wifiClient;
 #define TCP_IP "192.168.1.0" //server被分配到的IP
 #define TCP_PORT 8888
 
-#define deviceID 22
+#define ID 1 //1~94
+
+
+
 
 //pin config
+//input
+int analog_value = A0;
+int use_analog = D8; //D8 IO, 10k Pull-down, SS
+int press_btn = D2;
+//output
 int in_msg = D0;
-int out_msg = D2;
 int wifi_connect = D5;
 
-
+ int deviceID;
 char _buffer[4];
 
 //Initial
@@ -26,8 +33,12 @@ void setup() {
 
    pinMode(in_msg, OUTPUT);
    pinMode(wifi_connect, OUTPUT);
-   pinMode(out_msg, INPUT_PULLUP);
 
+   pinMode(analog_value, INPUT);
+   pinMode(use_analog, INPUT);
+   pinMode(press_btn, INPUT_PULLUP);
+   
+   deviceID = ID + 32;
    Serial.begin(9600);
    while (!Serial)
     ;
@@ -37,7 +48,8 @@ void setup() {
     Serial.print("Retry Connecting...SSID:");
     Serial.println(SSID);
     delay(100);
-   } Serial.println("Conneted to AP");
+   }
+   Serial.println("Conneted to AP");
    
    // check for the presence of the shield
    if ( WiFi.status() == WL_NO_SHIELD) {
@@ -50,18 +62,18 @@ void setup() {
       Serial.println(TCP_PORT);
     };
     Serial.println("Conneted to Server");
-   
+
    //set client ID
    _buffer[0] = 'C';
 
    //first char is character, second and third are ID
-   String(deviceID, HEX).toCharArray((_buffer + 1), 4);
-    
+//   String(deviceID, HEX).toCharArray((_buffer + 1), 4);
+     _buffer[1]=deviceID;
    //send to server
-   wifiClient.write((const uint8_t *)_buffer, 4);
+   wifiClient.write(&_buffer[0], 2);
 
    //clear buffer
-   for (int i = 1; i < sizeof(_buffer); i ++) {
+   for (int i = 0; i < sizeof(_buffer); i ++) {
     _buffer[i] = {0};
    }
    
@@ -73,28 +85,38 @@ static int i = 0;
 static unsigned long currentTimer = millis();
 void loop() {
 
-  
+  _buffer[0]=deviceID;
   //Trigger event by pressed button for 0.2 sec
-  int first = digitalRead(out_msg);
+  int first = digitalRead(press_btn);
   delay(200);
-  int second = digitalRead(out_msg);
+  int second = digitalRead(press_btn);
    
   if(!first){
+    //press_btn
     if(first != second){
-      
-      //first char is ID
-      String(deviceID, HEX).toCharArray(_buffer, 4);
-
-      //event trigger
-      _buffer[2] = '1';
-      wifiClient.write((const uint8_t *)_buffer, 4);
-
-      //clear buffer
+      _buffer[1] = '1'; 
+    }
+    //use Analog pin
+    if(digitalRead(use_analog)){ 
+    int a_value = analogRead(analog_value);
+    a_value = a_value/8;
+    _buffer[2]=a_value;
+    wifiClient.write(&_buffer[0], 4);
+    }
+  }else if(digitalRead(use_analog)){
+    int a_value = analogRead(analog_value);
+    a_value = a_value/8; // 1024 -> 128 for ascii
+    _buffer[2]=a_value;
+    
+    // send data
+    wifiClient.write(&_buffer[0], 4);
+    }
+    
+    
+    //clear buffer
       for(int i = 1; i < sizeof(_buffer); i++){
         _buffer[i] = {0};
       }
-    }
-  }
 
   //If no incoming data, .available() = -1.  
   if (wifiClient.available() > 0) {
@@ -114,9 +136,8 @@ void loop() {
     }
   }
   
-  if(millis() - currentTimer >= 10000) {
+  if(millis() - currentTimer >= 50000) {
     if(!wifiClient.connected()){
-
       //reconnect
       setup();
     } else {
